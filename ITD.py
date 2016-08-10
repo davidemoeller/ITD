3a996c8086a6fce18320ee8e7892aeb175c1d3d6
David Moeller <moel11@cancer-f5rdy12.osumc.edu>
2016-08-10 13:06:33 -0400

    Added main function to organize program better

--- ITD.py
+++ ITD.py
@@ -18,50 +18,52 @@ def build_check_query(length, index, query_list, reference_list):
         return duplicate_matches[-1]
     else:
         return []
+
+def main():
     
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', '--filename')
+    parser.add_argument('-f', '--filename')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-query = ""
-reference = ""
-
-with open(args.filename, 'r') as f:
-    for line in f:
-        #pdb.set_trace()
-        if not query:
-            query = line
-        else:
-            reference = line
-
-mismatch = 0;
+    query = ""
+    reference = ""
+    mismatch = 0
+    
+    with open(args.filename, 'r') as f:
+        for line in f:
+            if not query:
+                query = line
+            else:
+                reference = line
             
-for i in range(len(query)):
-    if (list(query)[i] != list(reference)[i]) and (list(query)[i] != '-'):
-        mismatch += 1
+            
+    for i in range(len(query)):
+        if (list(query)[i] != list(reference)[i]) and (list(query)[i] != '-'):
+            mismatch += 1
 
 
-#pdb.set_trace()
-percent_match = (len(query) - mismatch) / float(len(reference)) * 100
-print("{0:.2f}% match".format(percent_match))
 
-if percent_match < 70:
-    print("Match needs to be at least 70%")
-    exit(1)
+    percent_match = (len(query) - mismatch) / float(len(reference)) * 100
+    print("{0:.2f}% match".format(percent_match))
 
-i = 0
-while i < (len(reference)):
-    if list(reference)[i] == '-':
-        for j in range(i, len(reference)):
-            if list(reference)[j] != '-':
-                length_of_mystery = j - (i - 1)
-                #stop()
-                built_strand =  build_check_query(length_of_mystery, i, query, reference)
-                if any(built_strand):
-                    print(built_strand)
-    
-                i += j + 1
-                break
-    i += 1
+    if percent_match < 70:
+        print("Match needs to be at least 70%")
+        exit(1)
+
+    i = 0
+    while i < (len(reference)):
+        if list(reference)[i] == '-':
+            for j in range(i, len(reference)):
+                if list(reference)[j] != '-':
+                    length_of_mystery = j - (i - 1)
+                    built_strand =  build_check_query(length_of_mystery, i, query, reference)
+                    if any(built_strand):
+                        print(built_strand)
+                    i += j + 1
+                    break
+        i += 1
+
+if (__name__ == "__main__"):
+    main()
